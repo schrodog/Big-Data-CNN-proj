c5123fb3576d6e5451ff9ea54c335e5797db19e1
schrodog <sky_sun_tg@hotmail.com>
2018-05-01 10:00:54 +0800

    new norm

--- src/main2_2.py
+++ src/main2_2.py
@@ -11,14 +11,15 @@ binary format: [1,1024,1024,1024]
 '''
 
 DATA_DIR = os.path.join(os.getcwd(), '..', 'cifar-10-batches-bin')
-FILE_RUN = 0
+FILE_RUN = 50
+MODEL_NUM = 50
 
 NUM_EPOCHS = 10000
 NUM_CLASS = 10
 NUM_EXAMPLE_TRAIN = 50000
-BATCH_SIZE = 128
-learning_rate = 0.00005
-DECAY_EPOCH = 3000
+BATCH_SIZE = 100
+learning_rate = 0.002
+DECAY_EPOCH = 8000
 DECAY_FACTOR = 0.96
 # keep_prob = tf.placeholder(tf.float32)
 
@@ -57,7 +58,7 @@ def distorted_input(data_dir, batch_size, mode):
     if mode == 'train':
         filenames = [os.path.join(data_dir, 'data_batch_'+str(i)+'.bin') for i in range(1,6) ]
     elif mode == 'validation':
-        filenames = [os.path.join(data_dir, 'data_batch_5.bin') ]
+        filenames = [os.path.join(data_dir, 'data_batch_'+str(i)+'.bin') for i in range(1,6) ]
     elif mode == 'test':
         filenames = [os.path.join(data_dir, 'test_batch.bin') ]
 
@@ -68,7 +69,7 @@ def distorted_input(data_dir, batch_size, mode):
     if mode == 'train' or mode == 'validation':
         file_list = tf.train.string_input_producer(filenames, num_epochs=NUM_EPOCHS)
     else:
-        file_list = tf.train.string_input_producer(filenames, num_epochs=1)
+        file_list = tf.train.string_input_producer(filenames, num_epochs=NUM_EPOCHS)
 
     image_data, label  = read_input(file_list)
 
@@ -85,7 +86,9 @@ def distorted_input(data_dir, batch_size, mode):
         # read_input_data = read_input(file_list)
         reshaped_image = tf.cast(image_data, tf.float32)
         # crop a section of the image
+        # reshaped_image = tf.image.resize_images(reshaped_image, [64, 64] )
         # distorted_image = tf.random_crop(reshaped_image, [32, 32, 3])
+        # distorted_image = tf.image.resize_image_with_crop_or_pad(reshaped_image, 32, 32)
         # flip the image horizontally
         distorted_image = tf.image.random_flip_left_right(reshaped_image)
         # randomize the order of the operation
@@ -97,9 +100,9 @@ def distorted_input(data_dir, batch_size, mode):
         float_image.set_shape([32, 32, 3])
         label.set_shape([1])
         # random shuffling
-        min_fraction_example = 0.4
+        min_fraction_example = 1.0
         min_list_examples = int(NUM_EXAMPLE_TRAIN * min_fraction_example)
-        print ('Filling queue with %d CIFAR images' % min_list_examples)
+        # print ('Filling queue with %d CIFAR images' % min_list_examples)
 
         return generate_input(float_image, label, min_list_examples, batch_size, shuffle=True)
 
@@ -121,80 +124,120 @@ def _conv2d(inputs, kernel, strides, padding='SAME', name='conv'):
 def _pool(inputs, ksize, strides, padding='SAME', name='pool'):
     return tf.nn.max_pool(inputs, ksize=ksize, strides=strides, padding=padding, name=name)
 
-def _activation(conv, bias, name='activation', norm=True, mode='train'):
-    mult_add = tf.add(conv,bias)
-    ema = tf.train.ExponentialMovingAverage(decay=0.5)
-    if norm:
-        fc_mean, fc_var = tf.nn.moments(mult_add, axes=[0,1,2])
-        if mode == 'train':
-            pass
-        else:
-            ema_apply_op = ema.apply([fc_mean, fc_var])
-            with tf.control_dependencies([ema_apply_op]):
-                fc_mean, fc_var = tf.identity(fc_mean), tf.identity(fc_var)
-
-        scale = tf.Variable(tf.ones( [bias.shape.as_list()[0]] ))
-        shift = tf.Variable(tf.ones( [bias.shape.as_list()[0]] ))
-        epsilon = 0.001
-        mult_add = tf.nn.batch_normalization(mult_add, fc_mean, fc_var, shift, scale, epsilon)
+def _activation(mult_add, name='activation', norm=False, mode='train', fc=False):
+    # mult_add = tf.add(conv,bias)
+    # ema = tf.train.ExponentialMovingAverage(decay=0.5)
+    #
+    # mean_var = tf.Variable(tf.zeros( [bias.shape.as_list()[0]] ))
+    # variance_var = tf.Variable(tf.ones( [bias.shape.as_list()[0]] ))
+    # # batch normalization
+    # if norm:
+    #     scale = tf.Variable(tf.ones( [bias.shape.as_list()[0]] ))
+    #     shift = tf.Variable(tf.ones( [bias.shape.as_list()[0]] ))
+    #     epsilon = 0.001
+    #
+    #
+    #     if fc:
+    #         fc_mean, fc_var = tf.nn.moments(mult_add, axes=[0])
+    #     else:
+    #         fc_mean, fc_var = tf.nn.moments(mult_add, axes=[0,1,2])
+    #
+    #     # fc_mean, fc_var = tf.identity(fc_mean), tf.identity(fc_var)
+    #
+    #     # if mode == 'train' or mode == 'test':
+    #     #     pass
+    #     # else:
+    #     #     ema_apply_op = ema.apply([fc_mean, fc_var])
+    #     #     with tf.control_dependencies([ema_apply_op]):
+    #     #         fc_mean, fc_var = tf.identity(fc_mean), tf.identity(fc_var)
+    #     if mode=='train':
+    #         # add value to variables
+    #         assign_mean = mean_var.assign(fc_mean)
+    #         assign_var = variance_var.assign(fc_var)
+    #         ema_apply_op = ema.apply([mean_var, variance_var])
+    #
+    #         with tf.control_dependencies([assign_mean, assign_var]):
+    #             # mult_add = tf.nn.batch_normalization(mult_add, fc_mean, fc_var, shift, scale, epsilon)
+    #             mult_add = tf.nn.batch_norm_with_global_normalization(mult_add, fc_mean, fc_var, shift, scale, epsilon, True)
+    #     else:
+    #         # ema_apply_op = ema.apply([mean_var, variance_var])
+    #         mean = ema.average(mean_var)
+    #         variance = ema.average(variance_var)
+    #         local_scale, local_shift = tf.identity(scale), tf.identity(shift)
+    #         # mult_add = tf.nn.batch_normalization(mult_add, mean, variance, local_shift, local_scale, epsilon)
+    #         mult_add = tf.nn.batch_norm_with_global_normalization(mult_add, mean, variance, local_shift, local_scale, epsilon, True)
 
     return tf.nn.relu(mult_add, name=name)
 
 
 def cnn_network(input_x, mode):
 
+    if mode == 'train': #or mode=='validation':
+        bn_training = True
+    else:
+        bn_training = True
+
     with tf.variable_scope("layer1", reuse=tf.AUTO_REUSE):
-        filters1 = _weighted_variable([5,5,3,64])
+        filters1 = _weighted_variable([3,3,3,64])
         conv1 = _conv2d(input_x, filters1, [1,1,1,1])
         bias1 = _bias_variable([64])
-        activ1 = _activation(conv1, bias1, mode=mode)
+        # bn1 =
+        bn1 = tf.layers.batch_normalization(conv1 + bias1, momentum=0.9, training=bn_training)
+        activ1 = _activation(bn1, mode=mode)
 
         pool1 = _pool(activ1, ksize=[1,3,3,1], strides=[1,2,2,1])
-        tf.summary.histogram('layer1', pool1)
+        # tf.summary.histogram('layer1', pool1)
 
     with tf.variable_scope("layer2", reuse=tf.AUTO_REUSE):
-        filters2 = _weighted_variable([5,5,64,64])
+        filters2 = _weighted_variable([3,3,64,64])
         conv2 = _conv2d(pool1, filters2, [1,1,1,1])
         bias2 = _bias_variable([64])
-        activ2 = _activation(conv2, bias2, mode=mode)
+
+        bn2 = tf.layers.batch_normalization(conv2 + bias2, momentum=0.9, training=bn_training)
+        activ2 = _activation(bn2, norm=False, mode=mode)
 
         pool2 = _pool(activ2, ksize=[1,3,3,1], strides=[1,2,2,1])
-        tf.summary.histogram('layer2', pool2)
+        # tf.summary.histogram('layer2', pool2)
 
     with tf.variable_scope("layer3", reuse=tf.AUTO_REUSE):
         # [b,16,16,128]
         filters3 = _weighted_variable([3,3,64,64])
         conv3 = _conv2d(pool2, filters3, [1,1,1,1])
         bias3 = _bias_variable([64])
-        activ3 = _activation(conv3, bias3)
+
+        bn3 = tf.layers.batch_normalization(conv3 + bias3, momentum=0.9, training=bn_training)
+        activ3 = _activation(bn3, mode=mode)
         # [b,16,16,128]
-        pool3 = _pool(activ3, ksize=[1,3,3,1], strides=[1,1,1,1])
-        tf.summary.histogram('layer3', pool3)
+        pool3 = _pool(activ3, ksize=[1,3,3,1], strides=[1,2,2,1])
+        # drop3 = tf.nn.dropout(pool3, keep_prob=0.5) #keep_prob usually 0.5 or 0.3
+    #     # tf.summary.histogram('layer3', pool3)
 
     # with tf.variable_scope("layer4", reuse=tf.AUTO_REUSE):
     #     # [b,8,8,196]
-    #     filters4 = _weighted_variable([3,3,128,256])
+    #     filters4 = _weighted_variable([3,3,64,64])
     #     conv4 = _conv2d(pool3, filters4, [1,1,1,1])
-    #     bias4 = _bias_variable([256])
-    #     activ4 = _activation(conv4, bias4)
+    #     bias4 = _bias_variable([64])
+    #     activ4 = _activation(conv4, bias4, mode=mode)
     #     # [b,8,8,196]
     #     pool4 = _pool(activ4, ksize=[1,2,2,1], strides=[1,1,1,1])
     #     tf.summary.histogram('layer4', pool4)
 
     with tf.variable_scope("fc1", reuse=tf.AUTO_REUSE):
-        n = 8; m = 64
+        n = 4; m = 64
         reshape = tf.reshape(pool3, [-1, n*n*m])
         w_fc1 = _weighted_variable([n*n*m, 1024])
         b_fc1 = _bias_variable([1024])
-        # flat = tf.contrib.layers.flatten(pool2)
-	    #activ4 = tf.nn.relu(tf.matmul(reshape, weight4) + bias4) # choose which activ4?
 
-        # [b,384]
-        activ_fc1 = tf.nn.relu(tf.matmul(reshape, w_fc1) + b_fc1)
-        tf.summary.histogram('fc1', activ_fc1)
+        # activ_fc1 = tf.nn.relu(tf.matmul(reshape, w_fc1) + b_fc1)
+
+        bn_fc1 = tf.layers.batch_normalization(tf.matmul(reshape, w_fc1) + b_fc1, momentum=0.9, training=bn_training)
+        activ_fc1 = tf.nn.relu(bn_fc1)
+
+        # activ_fc1 = _activation( tf.matmul(reshape, w_fc1), b_fc1, mode=mode, fc=True)
+        # tf.summary.histogram('fc1', activ_fc1)
 
         # fc1 = tf.layers.dense(flat, 384)
-        # drop5 = tf.nn.dropout(fc1, keep_prob=0.5) #keep_prob usually 0.5 or 0.3
+        # drop5 = tf.nn.dropout(activ_fc1, keep_prob=0.5) #keep_prob usually 0.5 or 0.3
 
     # with tf.variable_scope("fc2", reuse=tf.AUTO_REUSE):
     #     # reshape = tf.reshape(pool4, [input_x.shape.as_list()[0], -1])
@@ -221,7 +264,7 @@ def cnn_network(input_x, mode):
         bias5 = _bias_variable([NUM_CLASS])
         softmax = tf.nn.softmax(tf.matmul(activ_fc1, weight5) + bias5)
 
-        tf.summary.histogram('softmax', softmax)
+        # tf.summary.histogram('softmax', softmax)
 
 
     return softmax
@@ -241,9 +284,12 @@ global_step = tf.train.get_or_create_global_step()
 def train(losses, learning_rate):
     lr = tf.train.exponential_decay(learning_rate, global_step, DECAY_EPOCH, DECAY_FACTOR, staircase=True)
     tf.summary.scalar('learning rate', lr)
-    train_op = tf.train.AdamOptimizer(lr).minimize(losses, global_step=global_step)
 
-    return train_op
+    update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+    with tf.control_dependencies(update_ops):
+        train_op = tf.train.AdamOptimizer(lr).minimize(losses, global_step=global_step)
+        return train_op, update_ops
+
 
 # accuracy
 def accuracy_fn(input_x, input_y):
@@ -257,7 +303,7 @@ def accuracy_fn(input_x, input_y):
 def model_fn(features,labels, mode):
 
     logits_train = cnn_network(features, mode='train')
-    logits_test = cnn_network(features, mode='validation')
+    logits_test = cnn_network(features, mode='test')
 
     predict_class = tf.argmax(logits_test, axis=1)
     # predict_prob = tf.nn.softmax(logits_test)
@@ -267,7 +313,7 @@ def model_fn(features,labels, mode):
         # loss_op = loss(logits_train, labels)
         # train_op = train(loss_op, learning_rate=learning_rate)
 
-    if mode == 'test':
+    if mode == 'test' or mode=='validation':
         accuracy_op = accuracy_fn(predict_class, labels)
         return accuracy_op
 
@@ -276,26 +322,45 @@ def model_fn(features,labels, mode):
 
 # training
 def main(argv=None):
-    count = 0
+    count = 0; high_count = 0
 
     # train
     image_data_train, label_train = distorted_input(DATA_DIR, BATCH_SIZE, 'train')
     logits_train, label_trains = model_fn(image_data_train, label_train, 'train')
     loss_op, lossx, lossy = loss_fn(logits_train, label_trains)
-    train_op = train(loss_op, learning_rate=learning_rate)
+    train_op, update_op = train(loss_op, learning_rate=learning_rate)
+
+    # test
+    image_data_test, label_test = distorted_input(DATA_DIR, BATCH_SIZE, 'test')
+    predict = cnn_network(image_data_test,'test')
+
+    top_k = tf.nn.in_top_k(predict, label_test, 1)
+    top_k = tf.reduce_sum(tf.cast(top_k, tf.float32))/10000
 
     # validate
     image_data_valid, label_valid = distorted_input(DATA_DIR, BATCH_SIZE, 'validation')
     accuracy_op = model_fn(image_data_valid, label_valid, 'test')
-    # accuracy_op = accuracy(predict_class, label_test2)
+    # accuracy_op = model_fn(image_data_train, label_train, 'validation')
+    # accuracy_op = accuracy_fn(predict_class, label_test)
 
     summaries = tf.summary.merge_all()
+
+    # keep moving mean,var for norm
+    # var_list = tf.trainable_variables()
+    # g_list = tf.global_variables()
+    # bn_moving_vars = [g for g in g_list if 'moving_mean' in g.name]
+    # bn_moving_vars += [g for g in g_list if 'moving_variance' in g.name]
+    # var_list += bn_moving_vars
+
+    # saver = tf.train.Saver(var_list=var_list, max_to_keep=5)
     saver = tf.train.Saver()
     # loss_op = loss_fn(logits_train, label_train)
 
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         sess.run(tf.local_variables_initializer())
+        # saver.restore(sess, os.path.join(os.getcwd(), '..', 'model', 'model46.ckpt'))
+
 
         writer = tf.summary.FileWriter("logs/run"+str(FILE_RUN), sess.graph)
 
@@ -308,6 +373,7 @@ def main(argv=None):
                 count +=1
 
                 sess.run(train_op)
+                sess.run(update_op)
                 summ = sess.run(summaries)
                 writer.add_summary(summ, global_step=sess.run(global_step))
 
@@ -320,8 +386,15 @@ def main(argv=None):
                     print('step:', count, 'loss:',sess.run(loss_op))
                         # print(sess.run(tf.shape(tf.get_variable('weights'))))
 
+                acc_value = sess.run(accuracy_op)
+                # if acc_value > 0.9:
+                #     high_count += 1
+                #     if high_count == 2:
+                #         break
+
                 if count%50 == 0:
-                    print('accuracy: ',sess.run(accuracy_op))
+                    top_k_value = sess.run(top_k)
+                    print('accuracy: ',acc_value, top_k_value )
 
         except tf.errors.OutOfRangeError:
             print('Done')
@@ -330,32 +403,33 @@ def main(argv=None):
             coord.request_stop()
             coord.join(threads)
 
-            saver_path = saver.save(sess, os.path.join(DATA_DIR,'..', 'model' , 'model5.ckpt'))
+            saver_path = saver.save(sess, os.path.join(DATA_DIR,'..', 'model' , 'model'+str(MODEL_NUM)+'.ckpt'))
             print("Model saved in path:", saver_path)
 
 
 
+
 # testing performance
-def eval_fn():
+def eval_fn(num):
     image_data_test, label_test = distorted_input(DATA_DIR, BATCH_SIZE, 'test')
     predict = cnn_network(image_data_test,'test')
 
     # top_k metrics
-    top_k = tf.nn.in_top_k(predict, label_test, 2)
-    top_k = tf.reduce_sum(tf.cast(top_k, tf.float32)) / BATCH_SIZE
+    top_k = tf.nn.in_top_k(predict, label_test, 1)
+    top_k = tf.reduce_sum(tf.cast(top_k, tf.float32)) / 10000
     # RMSE
     predict_class = tf.cast(tf.argmax(predict, axis=1), dtype=tf.int32)
     rmse = tf.metrics.mean_squared_error(label_test, predict_class)
     # confusion matrix
     confu_mat = tf.confusion_matrix(label_test, predict_class)
 
-    # tf.reset_default_graph()
-    # saver = tf.train.import_meta_graph(os.path.join(os.getcwd(), '..', 'model', 'model2.ckpt.meta'))
+    # ema = tf.train.ExponentialMovingAverage(decay=0.5)
+    # variables_to_restore = ema.variables_to_restore()
     saver = tf.train.Saver()
 
     with tf.Session() as sess:
         sess.run(tf.local_variables_initializer())
-        saver.restore(sess, os.path.join(os.getcwd(), '..', 'model', 'model0.ckpt'))
+        saver.restore(sess, os.path.join(os.getcwd(), '..', 'model', 'model'+str(num)+'.ckpt'))
 
 
         coord = tf.train.Coordinator()
@@ -365,6 +439,9 @@ def eval_fn():
 
         try:
             while not coord.should_stop():
+
+                # print(sess.run(label_test))
+                # print(sess.run(predict_class))
 
                 top_k_value = sess.run(top_k)
                 rmse_value = sess.run(rmse)[0]
@@ -388,5 +465,8 @@ def eval_fn():
 
 
 if __name__ == '__main__':
-    main()
-    # eval_fn()
+    # main()
+    eval_fn(50)
+
+
+
